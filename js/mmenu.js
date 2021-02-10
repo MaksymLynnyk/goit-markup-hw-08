40f86b6727585af1e5fc954bbc82cdb039566b04
MaksymLynnyk <72559264+MaksymLynnyk@users.noreply.github.com>
2021-02-10 22:32:46 +0200

    Add files via upload

---
+++ js/mmenu.js
@@ -0,0 +1,12 @@
+(() => {
+    burgerBtn = document.querySelector('[data-mmenu-burger]');
+    mmenu = document.querySelector('[data-mmenu]');
+
+    burgerBtn.addEventListener('click', () => {
+        expanded = burgerBtn.getAttribute("aria-expanded") === "true" || false;
+        burgerBtn.setAttribute("aria-expanded", !expanded);  
+
+        burgerBtn.classList.toggle('is-open');  
+        mmenu.classList.toggle('is-hidden');
+    });
+})();
